40da1e64c7c1571fa3214af3cdc0e358e9511b7d
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-11-19 01:38:53 -0300

    avances en la creacion de la api y la BD

--- src/app.py
+++ src/app.py
@@ -1,21 +1,21 @@
-import os
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_cors import CORS
-from dotenv import load_dotenv
 from models import db 
+import os
 
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['DEBUG'] = True,
-app.config['ENV'] = 'development'
-app.config['SQLALCHEMY_TRACK_MODIFICACTIONS'] =False
-app.config['SQLALCHEMY_DATABASEURI'] = os.getenv('DATABASEURI')
-
 db.init_app(app)
 Migrate(app, db)
+app.config['DEBUG'] = True,
+app.config['ENV'] = 'development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASEURI')
+
 
 @app.route('/', methods=['GET','POST', 'PUT', 'DELETE'])
 def main():
@@ -28,4 +28,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
