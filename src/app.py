8e2bf201bcb0ece4c863c3796e756fe24d2c6359
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-11-19 23:54:29 -0300

    ya esta conectada la base de datos conn las migraciones funcionando

--- src/app.py
+++ src/app.py
@@ -1,30 +1,28 @@
+import os
+from flask import Flask
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
-from flask_cors import CORS
+# from flask_cors import CORS
 from models import db 
-import os
 
 
 load_dotenv()
 
 app = Flask(__name__)
-db.init_app(app)
-Migrate(app, db)
 app.config['DEBUG'] = True,
 app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASEURI')
 
+db.init_app(app)
+migrate = Migrate(app, db)
+# CORS(app)
 
-@app.route('/', methods=['GET','POST', 'PUT', 'DELETE'])
+# aca se agregan los endpoints de la base de datos
+@app.route('/')
 def main():
-    data = {
-        "status": "server up"
-    }
-
-    return jsonify(data), 200
-
+    return jsonify({ "status": "Server Up"}), 200
 
 
 if __name__ == '__main__':
