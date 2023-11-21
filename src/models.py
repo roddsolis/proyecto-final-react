dd76e45384a3a568456835f999965c80e9f7c8d2
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-11-21 01:44:44 -0300

    se agrego el componente de boton y las variantes

--- src/models.py
+++ src/models.py
@@ -5,12 +5,42 @@ db = SQLAlchemy()
 
 
 class Alumno(db.Model):
-    __tablename__ = 'alumno'
+    __tablename__ = 'alumnos'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    email = db.Column(db.String)
-    password = db.Column(db.String)
+    nombre = db.Column(db.String(120), nullable=False)
+    apellidos = db.Column(db.String(120), nullable=False)
+    correo_electronico = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nulable=False, unique=True)
+  
 
+class Tutor(db.Model):
+    __tablename__ = 'tutores'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), nullable=False)
+    apellidos = db.Column(db.String(120), nullable=False)
+    correo_electronico = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nulable=False, unique=True)
 
-    
+class Perfil(db.Model):
+    __tablename__ = 'perfiles'
+    id = db.Column(db.Integer, primary_key=True)
+    profile_img = db.Column(db.String(240), nullable=True)
+
+class Materia(db.Model):
+    __tablename__ = 'materias'
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String, nullable=False, unique=True)
+
+class Solicitud_sala(db.Model):
+    __tablename__ = 'Solicitud_salas'
+    id = db.Column(db.Integer, primary_key=True)
+
+class Sala(db.Model):
+    __tablename__ = 'salas'
+    id = db.Column(db.Integer, primary_key=True)
+
+class Metodo_de_pago(db.Model):
+    __tablename__ = 'metodos_de_pago'
+
+class Cuenta_bancaria(db.Model):
+    __tablename__ = 'cuentas_bancarias'
