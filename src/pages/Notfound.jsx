c2cccef7e976cf0101c8b76fc8c1d289828dab4d
Fabian Yanez <fabian.yzb@gmail.com>
2023-11-22 14:43:44 -0300

    notfound css

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,21 +1,28 @@
+import "./../css/notFound.css";
 const Notfound = () => {
   return (
     <>
       <div id="error-page">
         <div className="content">
+          {/* CAMBIAR EL NUMERO DE ACUERDO AL ERROR */}
           <h2 className="header" data-text="404">
             404
           </h2>
           <h4 data-text="Ups! Pagina no encontrada">
             Ups! Pagina no encontrada
           </h4>
+          {/* CAMBIAR EL PARRAFO DE ACUERDO AL ERROR */}
           <p>
-            Disculpa, la pagina que buscas no existe. Si ves un
-            malfuncionamiento de algo, reporta el problema.
+            Disculpa, no sabemos que ha pasado. Si gustas, puedes reportar el
+            problema.
           </p>
-          <div className="btns">
-            <a href="#">volver a home</a>
-            <a href="#">reportar problema</a>
+          <div className="btns p-2">
+            <button type="button" className="btn btn-primary">
+              Volver a Home
+            </button>
+            <button type="button" className="btn btn-secondary">
+              Reportar Problema
+            </button>
           </div>
         </div>
       </div>
