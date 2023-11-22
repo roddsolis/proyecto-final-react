dbacca72e0871d0d9689fb06f13a786b023aff47
Fabian Yanez <fabian.yzb@gmail.com>
2023-11-22 11:57:45 -0300

    notfound actualizado

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,21 +1,25 @@
 const Notfound = () => {
   return (
-    <div id="error-page">
-      <div className="content">
-        <h2 className="header" data-text="404">
-          404
-        </h2>
-        <h4 data-text="Ups! Pagina no encontrada">Ups! Pagina no encontrada</h4>
-        <p>
-          Disculpa, la pagina que buscas no existe. Si ves un malfuncionamiento
-          de algo, reporta el problema.
-        </p>
-        <div className="btns">
-          <a href="#">volver a home</a>
-          <a href="#">reportar problema</a>
+    <>
+      <div id="error-page">
+        <div className="content">
+          <h2 className="header" data-text="404">
+            404
+          </h2>
+          <h4 data-text="Ups! Pagina no encontrada">
+            Ups! Pagina no encontrada
+          </h4>
+          <p>
+            Disculpa, la pagina que buscas no existe. Si ves un
+            malfuncionamiento de algo, reporta el problema.
+          </p>
+          <div className="btns">
+            <a href="#">volver a home</a>
+            <a href="#">reportar problema</a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
