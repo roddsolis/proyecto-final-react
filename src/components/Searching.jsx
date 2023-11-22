4afe4222101eb1a48a8a9cd45d45e0fc521b6076
Fabian Yanez <fabian.yzb@gmail.com>
2023-11-22 00:20:25 -0300

    Searching.jsx className corregido

--- src/components/Searching.jsx
+++ src/components/Searching.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Searching = () => {
-    return (
-        <>
-            <div class="searching">
-                <div class="text-left">
-                    Estamos buscando un tutor que haga match con el tema seleccionado. Esto puede tardar unos minutos...
-                </div>
-                <div class="text-right">Cancelar</div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="searching">
+        <div className="text-left">
+          Estamos buscando un tutor que haga match con el tema seleccionado.
+          Esto puede tardar unos minutos...
+        </div>
+        <div className="text-right">Cancelar</div>
+      </div>
+    </>
+  );
+};
 
 export default Searching;
