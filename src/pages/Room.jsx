1e2595019d932d46f0fe925657a07fd123f1eb5b
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-11-23 22:43:02 -0300

    trabaje en el modulo del chat y sus componentes

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -2,7 +2,7 @@ import StatusTag from "../components/StatusTag"
 import Avatar from "../components/Avatar"
 // import { FaMicrophone, FaVideo } from "react-icons/fa";
 // import { MdScreenShare, MdCall  } from "react-icons/md";
-import { MicOff,VideoOff, ScreenShare, Phone,  } from 'lucide-react';
+import { MicOff,VideoOff, ScreenShare, Phone, Plus  } from 'lucide-react';
 // import { Mic, Video  } from 'lucide-react';
 
 
@@ -12,22 +12,32 @@ const Room = () => {
     <>
     <div className="roomWrapper">
       <div className="callColumn">
-        <div className="userStatusWrapper"> <StatusTag status={'Online'}/> <h4 className='subtitle-l'>Héctor Valenzuela </h4> <p className='btn-text-m mb-0'>Tutor</p> </div>
+        <div className="userStatusWrapper"> <StatusTag status={'Online'}/> 
+          <h4 className='subtitle-sm'>Héctor Valenzuela </h4> 
+          <p className='btn-text-s mb-0'>Tutor</p> 
+        </div>
         <div className="videoWrapper">
-            {/* <Avatar />  */}
+            <Avatar /> 
               <div className="videoActionsWrapper"> 
-                <div className="callIconWrapper icon-active">{/* <Mic strokeWidth={2.5} /> */}<MicOff strokeWidth={2.5}/></div> 
-                <div className="callIconWrapper">{/* <Video strokeWidth={1.8} size={32}/> */}<VideoOff strokeWidth={1.8} size={32}/></div> 
-                <div className="callIconWrapper"><ScreenShare strokeWidth={2.5} /></div> 
-                <div className="callIconWrapper icon-red"><Phone strokeWidth={2.5} /></div> 
+                <div className="callIconWrapper icon-active">{/* <Mic strokeWidth={2.5} /> */}<MicOff /></div> 
+                <div className="callIconWrapper icon-active">{/* <Video strokeWidth={1.8}/> */}<VideoOff /></div> 
+                <div className="callIconWrapper"><ScreenShare/></div> 
+                <div className="callIconWrapper icon-red"><Phone /></div> 
               </div> 
         </div>
       </div>
       <div className="chatColumn">
         <div className="chatWrapper">
-          <div className="heaWrapper">head</div>
+          <div className="headWrapper"><p className='btn-text-s mb-0'>Conversación con Hector</p></div>
           <div className="mainWrapper">main</div>
-          <div className="actionsWrapper">send</div>
+          <div className="actionsWrapper">
+            <div className="chatActionContainer">
+              <div className="actionIconWraper"><Plus size={20}/></div>
+              <div className="inputWrapper">
+                <input type="text" placeholder='Envia un mensaje a Hector' className='paragraph-s'/>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
