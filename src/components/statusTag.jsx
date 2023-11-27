fac325516f73d365ff93124881a11d9074ac8305
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-11-27 16:49:38 -0300

    avanzando en la implementacion para hacer una conexion de video dentro de la sala

--- src/components/statusTag.jsx
+++ src/components/statusTag.jsx
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
-const StatusTag = ({ status, color }) => {
-=======
 
 
 const StatusTag = ({status, color}) => {
 
   
 
->>>>>>> 0ca38705425d3aae943f5d936217d88ea450ff7e
   return (
     <>
       <div className="statusWrapper">
         <div className={`statusLight ${color}`}></div>
-<<<<<<< HEAD
-        <p className="paragraph-m mb-0">
-          <strong>{status}</strong>
-        </p>
-=======
         <p className='paragraph-s mb-0'><strong>{status}</strong></p> 
->>>>>>> 0ca38705425d3aae943f5d936217d88ea450ff7e
       </div>
     </>
   );
