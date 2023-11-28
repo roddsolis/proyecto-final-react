db97e5b025851ce8491220d1883762a2bc5105a6
Fabian Yanez <fabian.yzb@gmail.com>
2023-11-28 04:31:17 -0300

    cambios a Cardprofile y TutorBankData

--- src/components/Categorias.jsx
+++ src/components/Categorias.jsx
@@ -6,41 +6,28 @@ const Categorias = () => {
     setBotonSeleccionado(nombreBoton);
   };
 
-  const nombresDeBotones = [
-    "JavaScript",
-    "HTML5",
-    "React",
-    "Node JS",
-    "CSS",
-    "Angular",
-    "Python",
-    "Next",
-    "Java",
-    "PHP",
-    "MySQL",
-    "TypeScript",
-  ];
+  const nombresDeBotones = ["JavaScript", "HTML5", "React", "Node JS", "CSS", "Angular", "Python", "Next", "Java", "PHP", "MySQL", "TypeScript"];
 
   return (
     <>
-      <div className="categoryWrapper container-fluid ">
+      <div className="categoryWrapper container-fluid mb-3">
         <h3 className="subtitle-sm">Selecciona una categoria</h3>
         <div className="chipsWrapper">
-        {nombresDeBotones.map((nombre, index) => (
-          <button
-            key={index}
-            onClick={() => handleBotonClick(nombre)}
-            style={{
-              padding: "0.6em 1.2em",
-              borderRadius: "100px",
-              border: "1px solid #e5ebef",
-              backgroundColor: botonSeleccionado === nombre ? "#e7effd" : "white",
-              color: botonSeleccionado === nombre ? "#0E4FC8" : "#708396",
-            }}
-          >
-            <strong>{nombre}</strong>
-          </button>
-        ))}
+          {nombresDeBotones.map((nombre, index) => (
+            <button
+              key={index}
+              onClick={() => handleBotonClick(nombre)}
+              style={{
+                padding: "0.6em 1.2em",
+                borderRadius: "100px",
+                border: "1px solid #e5ebef",
+                backgroundColor: botonSeleccionado === nombre ? "#e7effd" : "white",
+                color: botonSeleccionado === nombre ? "#0E4FC8" : "#708396",
+              }}
+            >
+              <strong>{nombre}</strong>
+            </button>
+          ))}
         </div>
       </div>
     </>
