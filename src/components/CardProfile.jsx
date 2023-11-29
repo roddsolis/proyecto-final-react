ec7e95eb4ae11acf2d629080b9e09e88c754c377
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-11-29 04:33:57 -0300

    se realizaron cambios en el home y la card de perfil

--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -1,33 +1,38 @@
-/* import Button from "./Button"; */
+import Button from "./Button";
 
 const CardProfile = () => {
   return (
     <>
-      <div className="card-profile">
-        <div className="card-profile-photo">
+      <div className="card-profile bg-white border rounded-4">
+
+        <div className="container d-flex flex-column align-items-center">
           <img className="card-profile-photo-img" src="https://picsum.photos/80" />
-          <p className="card-profile-photo-change">Cambiar</p>
+          <Button btnText={'cambiar'} className={'btn-tertiary btn-m'}/>
         </div>
-        <div className="card-profile-userdata">
-          <div className="subtitle-sm">Valentina Plaza</div>
-          <div className="card-profile-userdata-mail">valeplaza@gmail.com</div>
+
+        <div className="card-profile-userdata d-flex flex-column justify-content-center ">
+          <h5 className="subtitle-l text-center">Valentina Plaza</h5>
+          <p className='paragraph-m text-center'>valeplaza@gmail.com</p>
         </div>
-        <div className="card-profile-navbar">
-          <div className="card-profile-navbar-navlink1">
-            <div className="card-profile-text-left">Cambiar contraseña</div>
-            <div className="card-profile-text-right card-profile-blue">Cambiar</div>
+
+        <div className="container-fluid px-0 border rounded-3 border-gray">
+
+          <div className="d-flex align-items-center justify-content-between m-3">
+            <p className='paragraph-s mb-0'> Cambiar contraseña </p>
+            <Button btnText={'cambiar'} className={'btn-tertiary btn-m'}/>
           </div>
-          <div className="card-profile-navbar-navlink2">
-            <div className="card-profile-text-left">Método de pago</div>
-            <a href="/payingmethod" className="card-profile-text-right card-profile-blue">
-              Cambiar
-            </a>
+          <hr className='border border-grey'/>
+          <div className="d-flex align-items-center justify-content-between m-3">
+            <p className='paragraph-s mb-0'> Datos de pago </p>
+            <Button btnText={'cambiar'} className={'btn-tertiary btn-m'}/>
           </div>
+         
         </div>
-        <div className="card-profile-important text-left">
-          <p className="card-profile-important-text ">Para acceder a una sala y poder recibir una tutoría personalizada debes agregar un método de pago desde tu perfil.</p>
+
+        <div className="container-fuid p-4 d-flex align-items-center border rounded-3" id=''>
+          <p className="paragraph-s mb-0">Para acceder a una sala y poder recibir una tutoría personalizada debes agregar un método de pago desde tu perfil.</p>
         </div>
-        <div className="card-profile-logout">Cerrar sesión</div>
+        <Button btnText={'Cerrar sesion'} className={'btn-secondary btn-m'}/>
       </div>
     </>
   );
